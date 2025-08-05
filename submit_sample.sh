15446121fc219bc1e25f6b7252d31f0075fffb48
Zeviel Imani <zimani@holygpu7c26303.rc.fas.harvard.edu>
2025-08-05 15:21:11 -0400

    Fixed sampling script

--- submit_sample.sh
+++ submit_sample.sh
@@ -10,44 +10,44 @@
 
 export CUDA_VISIBLE_DEVICES=0
 
-checkpoint_path="edep_protons64_ldm/epoch=000040.ckpt"
+checkpoint_path="edep_protons64_ldm/runs/checkpoints/epoch=000144.ckpt"
 
 ## Conditional Sampling 
-# python3 scripts/txt2img.py \
-conda run -n ldm python3 scripts/txt2img.py \
+# conda run -n ldm python3 scripts/txt2img.py \
+python3 scripts/txt2img.py \
 	--base configs/latent-diffusion/protons64-ldm-kl.yaml \
-	--outdir one_mom_sample/edep_ldm_sample_bad_reco \
-	--px "199.1" \
-	--py "307.1" \
-	--pz "382.2" \
+	--outdir one_mom_sample/sample1 \
+	--px "314.0" \
+	--py "-126.4" \
+	--pz "249.1" \
 	--ddim_steps 200 \
 	--ddim_eta 0.0 \
 	--W 64 \
 	--H 64 \
 	--n_samples 128 \
-	--n_iter 100 \
+	--n_iter 1 \
 	--scale 5.0 \
 	--resume "$checkpoint_path"
 
-''' 
-# Momentums from bimodal validation dataset 
-bimodal sample1 = (314.0, -126.4, 249.1)
-bimodal sample2 = (292.5, 130.7, 70.8)
-bimodal sample3 = (-382.6, 2.8, -16.3)
-bimodal sample4 = (-391.4, -99.7, -217.9)
+### 
+# # Momentums from bimodal validation dataset 
+# bimodal sample1 = (314.0, -126.4, 249.1)
+# bimodal sample2 = (292.5, 130.7, 70.8)
+# bimodal sample3 = (-382.6, 2.8, -16.3)
+# bimodal sample4 = (-391.4, -99.7, -217.9)
 
-# Full Sample
-sampleA = 18.1, 242.8, 549.8
-sampleB = 19.6, -491.9, 533.7
+# # Full Sample
+# sampleA = 18.1, 242.8, 549.8
+# sampleB = 19.6, -491.9, 533.7
 
-# Bad Momentum Reco 
-bad_reco = 199.1, 307.1, 382.2
+# # Bad Momentum Reco 
+# bad_reco = 199.1, 307.1, 382.2
 
-# Directions
-Down right diag " 360.0, 168.6, -66.06"
-Down left diag: " 389.8, -245.2, -32.53"
-Straight up: "-382.5, 2.785, -16.33
-'''
+# # Directions
+# Down right diag " 360.0, 168.6, -66.06"
+# Down left diag: " 389.8, -245.2, -32.53"
+# Straight up: "-382.5, 2.785, -16.33
+### 
 
 ## Unconditional Sampling (possibly deprecated)
 # conda run -n ldm python3 -u scripts/sample_diffusion.py \
